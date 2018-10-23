907333cd54124044b44209bc8b58f7e8f335de6a
Veronika Kochetkova <veronica90k@gmail.com>
2018-10-23 14:36:18 -0700

    Dispatch an error when fetching products is failed

--- src/store/actions/productsActions.js
+++ src/store/actions/productsActions.js
@@ -1,20 +1,7 @@
 import * as actionTypes from './actionTypes';
 import axios from '../../axios-products';
 
-
-
-export const setProducts = (products) => {
-  return {
-    type: actionTypes.SET_PRODUCTS,
-    products: products
-  };
-};
-
-export const fetchProductsFailed = () => {
-  return {
-    type: actionTypes.FETCH_PRODUCTS_FAILED
-  };
-};
+/* axios functions */
 
 const getProducts = () => {
   return axios.get('/products.json');
@@ -38,12 +25,29 @@ const joinTables = (models, products) => {
   return res;
 }
 
+
+export const setProducts = (products) => {
+  return {
+    type: actionTypes.SET_PRODUCTS,
+    products: products
+  };
+};
+
+export const fetchProductsFailed = () => {
+  return {
+    type: actionTypes.FETCH_PRODUCTS_FAILED
+  };
+};
+
 export const fetchProducts = () => {
   return dispatch => {
     Promise.all([getModels(), getProducts()])
       .then(results => {
         const data = joinTables(results[0].data, results[1].data);
         dispatch(setProducts(data));
-    });
+      })
+      .catch(error => {
+        dispatch(fetchProductsFailed());
+      });
   };
 };
